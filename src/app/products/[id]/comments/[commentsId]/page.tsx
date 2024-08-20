b869deec562606d84047626b22eb61db01c956a5
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-08-20 21:59:37 +0200

    ✨ Use Routing dynamic > Add Shop

--- src/app/products/[id]/comments/[commentsId]/page.tsx
+++ src/app/products/[id]/comments/[commentsId]/page.tsx
@@ -1,9 +1,14 @@
-export default function ProductComment({params}: {
+import { notFound } from "next/navigation";
+
+export default function ProductComment({ params }: {
     params: {
         id: string;
         commentsId: string;
     }
 }) {
+    if (parseInt(params.id) > 50) {
+        notFound();
+    }
     return (
         <h1>Comment {params.commentsId} for product {params.id}</h1>
     );
