ba672440c5feb83d459e010ac8edf1f0c211f893
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-08-21 18:35:12 +0200

    ✨Route > Use useRouter

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,17 @@
+'use client';
+import { useRouter } from 'next/navigation';
+
 export default function Contact() {
+    const router = useRouter();
+
+    const handleSendContactForm = () => {
+        router.push('/');
+    }
+
     return (
-        <h1>Contact</h1>
+        <>
+            <h1>Contact</h1>
+            <button onClick={handleSendContactForm}>Valider le formulaire</button>
+        </>
     )
 }
